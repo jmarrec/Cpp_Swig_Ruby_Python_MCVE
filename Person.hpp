be706f61c4a5c9a156289d37ee6db9307ed34430
Julien Marrec <julien.marrec@gmail.com>
2021-02-16 15:38:44 +0100

    Move the method to the .i file to not pollute the cpp API.

--- Person.hpp
+++ Person.hpp
@@ -16,6 +16,7 @@ class Person {
 
     std::string getName() const;
     bool setName(const std::string& t_newName);
+
   private:
     std::string m_name;
 };
@@ -27,22 +28,6 @@ std::string personName(const Person& person);
 inline void setName(Person &p, const std::string &newname) {
   p.setName(newname);
 }
-
-// get an integral representation of the pointer that is this Person
-inline long long toInt(Person &p) {
-  std::clog << "original pointer: " << &p << '\n';
-  const auto result = reinterpret_cast<long long>(&p);
-  std::clog << "toInt from C++ " << result << '\n';
-  return result;
-}
-
-// take the integer from toInt and reinterpret_cast it back into a Person *, then return that as a reference
-inline Person &fromInt(long long i) {
-  auto *ptr = reinterpret_cast<Person *>(i);
-  std::clog << "Reclaimed pointer: " << ptr << '\n';
-  return *ptr;
-}
-
 
 } // Namespace Test
 
--- Person.i
+++ Person.i
@@ -20,6 +20,26 @@
     os << *$self;
     return os.str();
   }
+  #ifdef SWIGRUBY
+    // get an integral representation of the pointer that is this Person
+    inline long long __toInt() {
+      std::clog << "original pointer: " << $self << '\n';
+      const auto result = reinterpret_cast<long long>($self);
+      std::clog << "toInt from C++ " << result << '\n';
+      return result;
+    }
+
+
+  #endif
+
+  #ifdef SWIGPYTHON
+    // take the integer from toInt and reinterpret_cast it back into a Person *, then return that as a reference
+    static inline Test::Person& _fromInt(long long i) {
+      auto *ptr = reinterpret_cast<Test::Person *>(i);
+      std::clog << "Reclaimed pointer: " << ptr << '\n';
+      return *ptr;
+    }
+  #endif
 };
 
 #endif //PERSON_I
